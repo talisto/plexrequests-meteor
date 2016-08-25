f4aa554cfb6cfe72c82930ff0f5676c9bca4931c
Matt Lyon <talisto@gmail.com>
2016-08-25 02:24:58 -0700

    formatting (tabs to spaces)

--- server/methods/search/searchContent.js
+++ server/methods/search/searchContent.js
@@ -1,69 +1,69 @@
 Meteor.methods({
-	"searchContent": function(searchterm, searchType) {
-		check(searchterm, String);
-		check(searchType, String);
+  "searchContent": function(searchterm, searchType) {
+    check(searchterm, String);
+    check(searchType, String);
 
-        var type, result;
+    var type, result;
 
-        if (searchType === "Music" || searchType === "Albums" || searchType === "Artists") {
-            type = 'music';
-        } else if (searchType === "TV Shows") {
-            type = 'tv';
-        } else {
-            type = 'movie';
-        }
+    if (searchType === "Music" || searchType === "Albums" || searchType === "Artists") {
+        type = 'music';
+    } else if (searchType === "TV Shows") {
+        type = 'tv';
+    } else {
+        type = 'movie';
+    }
 
-		if (type === "tv") {
+    if (type === "tv") {
 
-			try {
-				result = Meteor.call("tvmaze", searchterm, type);
+      try {
+        result = Meteor.call("tvmaze", searchterm, type);
 
-			} catch (error) {
-				logger.error("tvmaze Error -> " + error.message);
-				return [];
+      } catch (error) {
+        logger.error("tvmaze Error -> " + error.message);
+        return [];
 
-			}
+      }
 
-		} else if (type === "movie") {
+    } else if (type === "movie") {
 
-			try {
-				result = Meteor.call("TMDBSearch", searchterm, type);
-				var tmp = result.link;
-				result.link = "https://image.tmdb.org/t/p/w184" + tmp;
+      try {
+        result = Meteor.call("TMDBSearch", searchterm, type);
+        var tmp = result.link;
+        result.link = "https://image.tmdb.org/t/p/w184" + tmp;
 
-			} catch (error) {
-				logger.error("TMDBSearch Error -> " + error.message);
-				return [];
+      } catch (error) {
+        logger.error("TMDBSearch Error -> " + error.message);
+        return [];
 
-			}
-		} else if (type === 'music') {
-			try {
-				result = MusicBrainz.searchGroupedRelease(searchterm);
-			} catch (error) {
-				logger.error("Headphones Error -> " + error.message);
-				return [];
+      }
+    } else if (type === 'music') {
+      try {
+        result = MusicBrainz.searchGroupedRelease(searchterm);
+      } catch (error) {
+        logger.error("Headphones Error -> " + error.message);
+        return [];
 
-			}
-		}
+      }
+    }
 
-		return result;
-	},
+    return result;
+  },
 
-	"searchOptions": function () {
-		var options = [];
+  "searchOptions": function () {
+    var options = [];
 
-		if (Settings.find({}).fetch()[0].searchOptionsMOVIES) {
-			options.push("Movies");
-		}
+    if (Settings.find({}).fetch()[0].searchOptionsMOVIES) {
+      options.push("Movies");
+    }
 
-		if (Settings.find({}).fetch()[0].searchOptionsTV) {
-			options.push("TV Shows");
-		}
+    if (Settings.find({}).fetch()[0].searchOptionsTV) {
+      options.push("TV Shows");
+    }
 
-		if (Settings.find({}).fetch()[0].searchOptionsMUSIC) {
-			options.push("Albums");
-		}
+    if (Settings.find({}).fetch()[0].searchOptionsMUSIC) {
+      options.push("Albums");
+    }
 
-		return options;
-	}
+    return options;
+  }
 });
